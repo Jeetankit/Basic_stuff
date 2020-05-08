a42dc99665e5be11143f6635098fe80af7892da7
svr <svr@pop-os.localdomain>
2020-05-08 14:36:48 +0530

    initial scrapping.

--- test1.py
+++ test1.py
@@ -11,10 +11,12 @@ def do_something(seconds):
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
     secs=[5,4,3,2,1]
-    results=[executor.submit(do_something, sec) for sec in secs]
+    results=executor.map(do_something,secs)
 
-    for f in concurrent.futures.as_completed(results):
-        print(f.result())
+    for result in results:
+        print(result)
+
+
 #threads=[]
 #for _ in range(10):
 #    t1=threading.Thread(target=do_something,args=[1.5])
