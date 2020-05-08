80a0520f4b669f37f0fb0410d1926a98b6c8f54f
svr <svr@pop-os.localdomain>
2020-05-08 13:12:58 +0530

    threading moduales.

--- test1.py
+++ test1.py
@@ -1,2 +1,30 @@
-print("helo world")
-print("testing")
+import time
+import concurrent.futures
+#start counting time
+start =time.perf_counter()
+
+
+def do_something(seconds):
+    print(f'Sleeping {seconds} second(s)...')
+    time.sleep(seconds)
+    return f"done sleeping.... {seconds},,second(s)"
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    secs=[5,4,3,2,1]
+    results=[executor.submit(do_something, sec) for sec in secs]
+
+    for f in concurrent.futures.as_completed(results):
+        print(f.result())
+#threads=[]
+#for _ in range(10):
+#    t1=threading.Thread(target=do_something,args=[1.5])
+#    t1.start()
+#    threads.append(t1)
+#
+#for th in threads:
+#    th.join()
+
+
+finish=time.perf_counter()
+
+print(f'finsihed in {round(finish-start,2)} second(s)')
