89e8b399c6b5673329e0c480b4c86415c12f547c
Jeetankit <jeet.ankit194@gmail.com>
2020-05-13 17:39:38 +0530

    Tried to write the content of futrue object in the file

--- test1.py
+++ test1.py
@@ -1,21 +1,21 @@
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 import os
 import time
 
 
 def task(n):
-    print(f"start of thread{n} ..... {os.getlogin()}")
-    time.sleep(5)
+    print(f"start of thread{n} ..... {os.getlogin()}......{os.ctermid()}")
+    time.sleep(3)
     print(f"end of the thread{n}......{os.getlogin()}")
 
 y=time.process_time()
 print(y)
 
 with ProcessPoolExecutor(5) as exexutor:
-    results=[exexutor.submit(task,_) for _ in range(6,0,-1)]
+    results=[exexutor.submit(task,_) for _ in range(5,0,-1)]
+
+with open("ankit.txt","a")as a:
+    a.write(str(results))
 
 
-print(str(time.process_time() - y))
-
-
-as
+#print(str(time.process_time() - y))
