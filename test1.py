e714b487cc43245eb5bc834884a64483c1110367
Jeetankit <jeet.ankit194@gmail.com>
2020-05-13 17:01:41 +0530

    nre multi threading imported

--- test1.py
+++ test1.py
@@ -1,27 +1,21 @@
-#this module provides a high level API for executing asynchronus task
-import concurrent.futures
-
-#executor class is an abstract class and cannot be used directly
-#The two subclass can be imported which are
-#Number 1-------
-concurrent.futures.ThreadPoolExecutor
-#This class has pools of th thread and use the multithreading
-
-concurrent.futures.ProcessPoolExecutor
-
-#This class has pools of process and use the muliporocessing
-#we get a pool of threads or process and submit task to these pool
-#it will automatically the assigne the task to the available resource and schedule them torun
+from concurrent.futures import ProcessPoolExecutor
+import os
+import time
 
 
-from time import sleep
+def task(n):
+    print(f"start of thread{n} ..... {os.getlogin()}")
+    time.sleep(5)
+    print(f"end of the thread{n}......{os.getlogin()}")
+
+y=time.process_time()
+print(y)
+
+with ProcessPoolExecutor(5) as exexutor:
+    results=[exexutor.submit(task,_) for _ in range(6,0,-1)]
 
 
-def task(message):
-    print("start task")
-    sleep(5)
-    print(message)
+print(str(time.process_time() - y))
 
-pool=concurrent.futures.ThreadPoolExecutor(3)
 
-future=pool.map(task,["hell0","nothing","couldbe"])
+as
