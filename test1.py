a2ceb21f8a708cf8260977587d854f1bb0723c88
Jeetankit <jeet.ankit194@gmail.com>
2020-05-14 07:40:39 +0530

    localtion changes

--- test1.py
+++ test1.py
@@ -1,21 +1,13 @@
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 import os
 import time
+import pygeocoder
 
 
 def task(n):
-    print(f"start of thread{n} ..... {os.getlogin()}......{os.ctermid()}")
+    print(f"start of thread{n} ..... ")
     time.sleep(3)
-    print(f"end of the thread{n}......{os.getlogin()}")
-
-y=time.process_time()
-print(y)
+    print(f"end of the thread{n}......")
 
 with ProcessPoolExecutor(5) as exexutor:
     results=[exexutor.submit(task,_) for _ in range(5,0,-1)]
-
-with open("ankit.txt","a")as a:
-    a.write(str(results))
-
-
-#print(str(time.process_time() - y))
